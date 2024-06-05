8cff3aa4f3ffd2fafdabf39b25ae71dd1e77b491
enghayle01 <mabdirahim832@gmail.com>
2024-06-05 11:35:57 +0300

    nexcent web

---
+++ main.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+        const mobileMenu = document.querySelector(".navigation-bar");
+        const icons = document.querySelectorAll("i");
+
+        hamburger.addEventListener("click", function (event) {
+            const isVisible = mobileMenu.getAttribute('data-visible');
+            if (isVisible == "true") {
+                // qari hiding
+                mobileMenu.setAttribute('data-visible', "false");
+                icons[0].setAttribute('data-visible', "true");
+                icons[1].setAttribute('data-visible', "false");
+            } else if (isVisible == "false") {
+                // soo bandhig showing
+                mobileMenu.setAttribute('data-visible', "true");
+                icons[0].setAttribute('data-visible', "false");
+                icons[1].setAttribute('data-visible', "true");
+            }
+        });
+
